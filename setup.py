c719487a4cea8a99be5158637946fa483c3da8eb
Rawand Hawiz (rhawiz) <rawandhawiz@gmail.com>
2021-06-24 20:44:47 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from setuptools import setup
 
 setup(
     name='pyiqfeed',
-    version='0.9',
+    version='0.9.1',
     description='Handles connections to IQFeed, the market data feed by DTN',
-    url='https://github.com/akapur/pyiqfeed',
+    url='https://github.com/rhawiz/pyiqfeed',
     author='Ashwin Kapur',
     author_email='ashwin.kapur@gmail.com',
-    license='GPL v2',
     packages=setuptools.find_packages(),
     install_requires=['numpy~=1.20.2'],
-
+    python_requires='>=3.7',
+    classifiers=[
+        "Development Status :: 3 - Alpha"
+    ],
 )
