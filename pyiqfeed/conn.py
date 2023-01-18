340aad094e857fe9b7b22cd67655af3d4eb6b17e
Mathieu Bartels <mathieubartels@gmail.com>
2023-01-18 14:49:58 +0100

    add function for requesting 5MD

--- pyiqfeed/conn.py
+++ pyiqfeed/conn.py
@@ -3315,6 +3315,37 @@ class LookupConn(FeedConn):
         else:
             return data
 
+    def request_5MD(
+        self,
+        security_type: str,
+        market: str = None,
+        timeout: int = None,
+    ) -> List[str]:
+
+        req_id = self._get_next_req_id()
+        self._setup_request_data(req_id)
+        req_cmd = "5MS,%s,%s,%s\r\n" % (
+            fr.blob_to_str(security_type),
+            fr.blob_to_str(market),
+            req_id,
+        )
+        self._send_cmd(req_cmd)
+        self._req_event[req_id].wait(timeout=timeout)
+        data = self._read_5MD(req_id)
+        if (len(data) == 2) and (data[0] == "!ERROR!"):
+            err_msg = "Request: %s, Error: %s" % (req_cmd, str(data[1]))
+            raise RuntimeError(err_msg)
+        else:
+            return data
+
+    def _read_5MD(self, req_id: str) -> List[str]:
+        """Read a buffer and return it as a futures chain."""
+        res = self._get_data_buf(req_id)
+        if res.failed:
+            return ["!ERROR!", res.err_msg]
+        else:
+            return res
+
     def request_futures_spread_chain(
         self,
         symbol: str,
